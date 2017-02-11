783135da2a30fcd9964505091b66f9afca37066e
Gregg Hanano <gregg@Unitys-MacBook-Air-106.local>
2017-02-10 17:58:03 -0800

    added the home template and imported render_template function

--- python-minicamp-homework-2/server.py
+++ python-minicamp-homework-2/server.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html');
+    return render_template('home.html')
 
 @app.route('/birthday')
 def birthday():
