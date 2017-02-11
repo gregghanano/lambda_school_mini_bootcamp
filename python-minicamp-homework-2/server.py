a3ba7b80615800d697293636db1ac5226229cf05
Gregg Hanano <gregg@Unitys-MacBook-Air-106.local>
2017-02-10 18:04:41 -0800

    added sum route to add two numbers in url

--- python-minicamp-homework-2/server.py
+++ python-minicamp-homework-2/server.py
@@ -14,3 +14,8 @@ def birthday():
 @app.route("/greeting/<name>")
 def greeting(name):
     return "Hello {}!".format(name)
+
+@app.route("/sum/<number1>/<number2>")
+def sum(number1, number2):
+    result = int(number1) + int(number2)
+    return number1 + " + " + number2 +" = " + str(result)
